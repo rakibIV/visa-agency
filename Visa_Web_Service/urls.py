e9730ff1c9195ad9da556687f6528f180d97c8eb
rakib <rakibhasan42008@gmail.com>
2026-07-20 17:43:47 +0600

    fix deployment error

--- Visa_Web_Service/urls.py
+++ Visa_Web_Service/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/docs/', permanent=False)),
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
