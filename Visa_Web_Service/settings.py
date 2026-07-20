8b06bc5f36f11e6a716c31b65612719f912214fb
rakib <rakibhasan42008@gmail.com>
2026-07-20 17:13:10 +0600

    fix deployment error 3

--- Visa_Web_Service/settings.py
+++ Visa_Web_Service/settings.py
@@ -301,3 +301,24 @@ if not DEBUG:
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
