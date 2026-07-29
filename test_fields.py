fb3a13bd8c34922cd92e61020365508952c2998b
rakib <rakibhasan42008@gmail.com>
2026-07-29 12:34:48 +0600

    Remove temporary test scripts, db fixtures, and load utility scripts from repository

--- test_fields.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Visa_Web_Service.settings')
-django.setup()
-
-from agency.serializers import ContactUsSerializer
-
-print("Meta read_only_fields:", getattr(ContactUsSerializer.Meta, 'read_only_fields', None))
